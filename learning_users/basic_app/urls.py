07abb26726c7fb6716dd0bd3aa228ce79430f7cd
unknown <secura37@gmail.com>
2018-08-02 22:45:05 +0200

    'first'

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from basic_app import views
+
+#template URLs
+app_name = 'basic_app'
+
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+
+
+]
